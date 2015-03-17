7469ef97f2c175eb9b1ce95909a6a77d1dd35259
aaraque <arevalo.a.05@gmail.com>
2015-03-17 16:42:09 -0430

    .

--- modulos/prestamos.php
+++ modulos/prestamos.php
@@ -7,7 +7,7 @@
       <div class="col-md-12">
             
 
-          <ul class="nav nav-tabs">
+          <ul class="nav nav-tabs" id="navegacion-busqueda">
             <li class="active" id="inx"><a class="text-danger" href="#tab_a" data-toggle="tab">Libros</a></li>
             <li><a href="#tab_b" class="text-danger" data-toggle="tab">Tesis</a></li>
             <li><a href="#tab_c" class="text-danger" data-toggle="tab">Materiales</a></li>
@@ -20,10 +20,10 @@
                     <div class="form-group">
                       <input type="hidden" name="tipo_recurso" value="libro" />
                       <input type="hidden" name="function" value="consultar_recurso" />
-                      <input type="text" class="form-control" size="10" id="txt_codigol" name="txt_codigol" placeholder="C&oacute;digo Libro" />
+                      <input type="text" class="form-control codigo" size="10" id="txt_codigol" name="txt_codigol" placeholder="C&oacute;digo Libro" />
                     </div>
                     <div class="form-group">
-                      <input type="text" class="form-control" size="40" id="txt_descripcionl" name="txt_descripcionl" placeholder="Descripci&oacute;n Libro" />
+                      <input type="text" class="form-control descripcion" size="40" id="txt_descripcionl" name="txt_descripcionl" placeholder="Descripci&oacute;n Libro" />
                     </div>
                     <button type="submit" id="btnl" class="btn btn-primary btn-form">Buscar&nbsp;Libro&nbsp;&nbsp;<i class="glyphicon glyphicon-search"></i></button>
                     <img src="images/cargando.gif" id="load-l" class="img-circle load-l" />
@@ -37,10 +37,10 @@
                     <div class="form-group">
                       <input type="hidden" name="tipo_recurso" value="tesis" />
                       <input type="hidden" name="function" value="consultar_recurso" />
-                      <input type="text" class="form-control" size="10" id="txt_codigot" name="txt_codigot" placeholder="C&oacute;digo Tesis" />
+                      <input type="text" class="form-control codigo" size="10" id="txt_codigot" name="txt_codigot" placeholder="C&oacute;digo Tesis" />
                     </div>
                     <div class="form-group">
-                      <input type="text" class="form-control" size="40" id="txt_descripciont" name="txt_descripciont" placeholder="Descripci&oacute;n Tesis" />
+                      <input type="text" class="form-control descripcion" size="40" id="txt_descripciont" name="txt_descripciont" placeholder="Descripci&oacute;n Tesis" />
                     </div>
                     <button type="submit" id="btnt" class="btn btn-primary btn-form">Buscar&nbsp;Tesis&nbsp;&nbsp;<i class="glyphicon glyphicon-search"></i></button>
                     <img src="images/cargando.gif" id="load-t" class="img-circle load-t" />
@@ -54,10 +54,10 @@
                     <div class="form-group">
                       <input type="hidden" name="tipo_recurso" value="material" />
                       <input type="hidden" name="function" value="consultar_recurso" />
-                      <input type="text" class="form-control" size="10" id="txt_codigom" name="txt_codigom" placeholder="C&oacute;digo Material" />
+                      <input type="text" class="form-control codigo" size="10" id="txt_codigom" name="txt_codigom" placeholder="C&oacute;digo Material" />
                     </div>
                     <div class="form-group">
-                      <input type="text" class="form-control" size="40" id="txt_descripcionm" name="txt_descripcionm" placeholder="Descripci&oacute;n Material" />
+                      <input type="text" class="form-control descripcion" size="40" id="txt_descripcionm" name="txt_descripcionm" placeholder="Descripci&oacute;n Material" />
                     </div>
                     <button type="submit" id="btnm" class="btn btn-primary btn-form">Buscar&nbsp;Material&nbsp;&nbsp;<i class="glyphicon glyphicon-search"></i></button>
                     <img src="images/cargando.gif" id="load-m" class="img-circle load-m" />
@@ -68,13 +68,11 @@
 
           <p>&nbsp;</p>
           <!-- Resultado de la busqueda -->
-          <div id="table-result">
-            <table class='table table-responsive table-bordered table-hover datatable dataTable'>
-              <thead>
-                <tr class='danger'><th class='col-lg-2'>C&oacute;digo</th><th class='col-lg-2'>Tipo</th><th class="col-lg-7">Descripci&oacute;n</th><th class="text-center col-lg-1">Acciones</th></tr>
+          <div id="table-result" class="table-responsive">
+            <table class='table table-bordered table-striped table-hover datatable dataTable' id="datatable" style='border-radius:5px;'>
+              <thead id="head-result">
               </thead>
               <tbody id='content-result'>
-                
               </tbody>
             </table>
           </div>
@@ -92,9 +90,42 @@
 <script src="plugins/dataTables/js/jquery.dataTables.js" type="text/javascript"></script>
 <script src="plugins/dataTables/js/datatables.js" type="text/javascript"></script>
 <script>
+  var create_datatable = function() {};
+  var control_datatable = 0;
   $(document).ready(function() {
     $('#menu-item li.active').removeClass('active');
     $('#liPrestamos').addClass('active');
+    // click sobre pestañas
+    $('#navegacion-busqueda li').click(function(){
+        if ($(this).not('.active')) {
+          $('#head-result').html('');
+          $('#content-result').html('');
+          $('#table-result').fadeOut();
+          $('.descripcion').val('');
+          $('.codigo').val('');
+        }
+    });
+    create_datatable = function() {
+        // Configuracion datatable
+        $('#datatable').dataTable({
+          "AaSorting": [[0, "asc"]],
+          "sPaginationType": "bs_normal",
+          ajax: "data.json"
+        }); 
+        $('.datatable').each(function(){
+          var datatable = $(this);
+          // SEARCH - Add the placeholder for Search and Turn this into in-line form control
+          var search_input = datatable.closest('.dataTables_wrapper').find('div[id$=_filter] input');
+          search_input.attr({'placeholder':'Filtrar Resultado','id':'btn_search_table'});
+          search_input.addClass('form-control input-sm');
+          // LENGTH - Inline-Form control
+          var length_sel = datatable.closest('.dataTables_wrapper').find('div[id$=_length] select');
+          length_sel.addClass('form-control input-sm');
+        });
+        $('#btn_search_table').attr('title','Filtrar resultado');
+        $('#btn_search_table').tooltip();
+    }
+
     // validacion de formulario 
     $('.btn-form').click(function(e){
       e.preventDefault();
@@ -103,6 +134,9 @@
       else {
         $.ajax({
           beforeSend: function(){
+            $('#head-result').html('');
+            $('#content-result').html('');
+            $('#table-result').fadeOut();
             $('#load-'+prefx).fadeIn();
           },
           type:     'POST',
@@ -111,37 +145,30 @@
           dataType: 'json',
           success: function (resp) {
             $('#load-'+prefx).fadeOut();
+            $('#txt_codigo'+prefx).val(''); $('#txt_descripcion'+prefx).val(''); 
             if (resp == 0) { 
               alertify.error('<b>No se encontraron datos</b>');
-              $('#txt_codigo'+prefx).val(''); $('#txt_descripcion'+prefx).val(''); 
             } else if(resp == 1){
               alertify.error('<b>Acceso denegado</b>');
             } else if(typeof(resp) == "object") {
+                  if (prefx == 'l') {
+                      $('#head-result').append('<tr class="danger"><th class=\'col-lg-1 text-center\'>C&oacute;digo</th><th class=\'col-lg-2 text-center\'>Autor</th><th class=\'col-lg-2 text-center\'>Editorial</th><th class=\'col-lg-2 text-center\'>Materia</th><th class=\'col-lg-3\'>Descripci&oacute;n</th><th class=\'col-lg-1 text-center\'>Fecha</th><th class=\'text-center col-lg-1\'>Solicitud(s)</th></tr>');
+                  } else if (prefx == 't') {
+                      $('#head-result').append('<tr class="danger"><th class=\'col-lg-1 text-center\'>C&oacute;digo</th><th class=\'col-lg-2 text-center\'>Materia</th><th class=\'col-lg-2 text-center\'>Autor</th><th class=\'col-lg-2\'>Titulo</th><th class=\'col-lg-1 text-center\'>Fecha Publicaci&oacute;n</th><th class=\'col-lg-1 text-center\'>Mensi&oacute;n</th><th class=\'text-center col-lg-1\'>Solicitud(s)</th></tr>');
+                  } else if (prefx == 'm') {}
               $.each(resp,function(index,array){
-                  console.log(array);
-                  //$('#content-result').append('<tr><td>'+ array.id+'' +'</td><td></td><td></td><td></td></tr>');
+                  if (prefx == 'l') {
+                      $('#content-result').append('<tr><td class=\'text-center\'>'+ array['id_libro'] +'</td><td class=\'text-center\'>'+ array['id_autor'].toUpperCase() +'</td><td class=\'text-center\'>'+ array['id_editorial'].toUpperCase() +'</td><td class=\'text-center\'>'+ array['id_materia'].toUpperCase() +'</td><td>'+ array['descripcion'].toUpperCase() +'</td><td class=\'text-center\'>'+ array['fecha_publicacion'] +'</td><td class=\'text-center\'><button class=\'btn btn-danger\'>Prestar&nbsp;&nbsp;<i class=\'glyphicon glyphicon-transfer\'></i></button></td></tr>');
+                  } else if (prefx == 't') {
+                      $('#content-result').append('<tr><td class=\'text-center\'>'+ array['id_tesis'] +'</td><td class=\'text-center\'>'+ array['id_materia'].toUpperCase() +'</td><td class=\'text-center\'>'+ array['id_autor_tesis'].toUpperCase() +'</td><td>'+ array['titulo'].toUpperCase() +'</td><td class=\'text-center\'>'+ array['fecha_publicacion'] +'</td><td class=\'text-center\'>'+ array['mension'].toUpperCase() +'</td><td class=\'text-center\'><button class=\'btn btn-danger\'>Prestar&nbsp;&nbsp;<i class=\'glyphicon glyphicon-transfer\'></i></button></td></tr>');
+                  } else if (prefx == 'm') {}
               });
+              $('#table-result').fadeIn();
+              if (control_datatable == 0) {create_datatable(); control_datatable++;}
             }
           }
         });
       }
     });
-
-    // Configuracion datatable
-    $('.datatable').dataTable({
-      "sPaginationType": "bs_normal"
-    }); 
-    $('.datatable').each(function(){
-      var datatable = $(this);
-      // SEARCH - Add the placeholder for Search and Turn this into in-line form control
-      var search_input = datatable.closest('.dataTables_wrapper').find('div[id$=_filter] input');
-      search_input.attr({'placeholder':'Buscar','id':'btn_search_table'});
-      search_input.addClass('form-control input-sm');
-      // LENGTH - Inline-Form control
-      var length_sel = datatable.closest('.dataTables_wrapper').find('div[id$=_length] select');
-      length_sel.addClass('form-control input-sm');
-    });
-    $('#btn_search_table').attr('title','Filtrar resultado');
-    $('#btn_search_table').tooltip();
   });
 </script>
--- modulos/response_ajax.php
+++ modulos/response_ajax.php
@@ -7,23 +7,71 @@
 		}
 
 		public function consultar_recurso () {
+			sleep(.7);
 			$tipo_recurso = array('libro'=>'tbl_libros','tesis'=>'tbl_tesis','material'=>'tbl_material');
 			$prefx  = substr($_POST['tipo_recurso'], 0,1); 
 			if ($prefx == 'l') {
-				$where  = ' WHERE'.(!empty($_POST['txt_codigo'.$prefx]))?(' id_'.$_POST['tipo_recurso'].'='.$_POST['txt_codigo'.$prefx]):(' ');
-				$where .= (!empty($_POST['txt_descripcion'.$prefx]))?(' descripcion ILIKE\'%'.$_POST['txt_descripcion'.$prefx].'%\''):('');
+				$where  = ' WHERE'.((!empty($_POST['txt_codigo'.$prefx]))?(' id_'.$_POST['tipo_recurso'].'=\''.$_POST['txt_codigo'.$prefx].'\' '):(' '));
+				if (!empty($_POST['txt_codigo']) && !empty($_POST['txt_descripcion'])) {
+					$where .= ' AND ';
+				}
+				$where .= ((!empty($_POST['txt_descripcion'.$prefx]))?(' descripcion ILIKE\'%'.$_POST['txt_descripcion'.$prefx].'%\''):(''));
 			} else if ($prefx == 't') {
-
+				$where  = ' WHERE'.((!empty($_POST['txt_codigo'.$prefx]))?(' id_'.$_POST['tipo_recurso'].'=\''.$_POST['txt_codigo'.$prefx].'\' '):(' '));
+				$where .= ((!empty($_POST['txt_descripcion'.$prefx]))?(' titulo ILIKE\'%'.$_POST['txt_descripcion'.$prefx].'%\''):(''));
 			} else if ($prefx == 'm') {
 
 			}
 
-			$result = $this->consultasbd->select($tipo_recurso[$tabla=$_POST['tipo_recurso']],$campos='*',$where='');
+			$result = $this->consultasbd->select($tipo_recurso[$tabla=$_POST['tipo_recurso']],$campos='*',$where);
 			
 			if ($this->consultasbd->num_rows($result) > 0) {
+				$response = array();
+				if ($prefx == 'l') {
+					while($array = $this->consultasbd->fetch_array($result)) {
+						// datos de autor
+						$res_autor = $this->consultasbd->select($tabla='tbl_autor',$campos='*',$where = ((!empty($array['id_autor']))?'WHERE id_autor=\''.$array['id_autor'].'\'':''));
+						$fetch_autor = $this->consultasbd->fetch_array($res_autor);
+						$autor = $fetch_autor['nombre'].' '.$fetch_autor['apellido'];
+						// datos de editorial
+						$res_editorial = $this->consultasbd->select($tabla='tbl_editorial',$campos='*',$where = ((!empty($array['id_editorial']))?'WHERE id_editorial=\''.$array['id_editorial'].'\'':''));
+						$fetch_editorial = $this->consultasbd->fetch_array($res_editorial);
+						$editorial = $fetch_editorial['nombre'].' '.$fetch_editorial['ciudad'];
+						// datos de materia
+						$res_materia = $this->consultasbd->select($tabla='tbl_materia',$campos='*',$where = ((!empty($array['id_materia']))?'WHERE id_materia=\''.$array['id_materia'].'\'':''));
+						$fetch_materia = $this->consultasbd->fetch_array($res_materia);
+						$materia = $fetch_materia['nombre_materia'];
 
-				$array = $this->consultasbd->fetch_array($result);
-				echo json_encode([$array]);
+						$array['id_autor'] = $autor;
+						$array['id_editorial'] = $editorial;
+						$array['id_materia'] = $materia;
+						$array['fecha_publicacion'] = date('d-m-Y',strtotime($array['fecha_publicacion']));
+						$response[] = $array;
+					}
+				} else if ($prefx == 't') {
+					while($array = $this->consultasbd->fetch_array($result)) {
+						// datos de materia
+						$res_materia = $this->consultasbd->select($tabla='tbl_materia',$campos='*',$where = ((!empty($array['id_materia']))?'WHERE id_materia=\''.$array['id_materia'].'\'':''));
+						$fetch_materia = $this->consultasbd->fetch_array($res_materia);
+						$materia = $fetch_materia['nombre_materia'];
+						// datos de autor
+						$res_autor = $this->consultasbd->select($tabla='tbl_autor_tesis',$campos='*',$where = ((!empty($array['id_autor_tesis']))?'WHERE id_autor_tesis=\''.$array['id_autor_tesis'].'\'':''));
+						$fetch_autor = $this->consultasbd->fetch_array($res_autor);
+						$autor = $fetch_autor['nombre'].' '.$fetch_autor['apellido'];
+						$mension = $fetch_autor['mension'];
+
+						$array['id_autor_tesis'] = $autor;
+						$array['id_materia'] = $materia;
+						$array['mension'] = $mension;
+						$array['fecha_publicacion'] = date('d-m-Y',strtotime($array['fecha_publicacion']));
+						$response[] = $array;
+					}
+				} else if ($prefx == 'm') {
+					while($array = $this->consultasbd->fetch_array($result)) {
+						$response[] = $array;
+					}
+				}
+				echo json_encode($response);
 			} else {
 				echo json_encode(0);
 			}
@@ -42,6 +90,6 @@
 			echo json_encode(1);
 		endif;
 	else:
-		echo json_encode(0);
+		echo 'Hola Mundo!';
 	endif;
 ?>
