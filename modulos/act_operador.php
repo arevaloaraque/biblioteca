c4734597d7fdb8ec1ef0a3a809e74c47fd15ddb6
aaraque <arevalo.a.05@gmail.com>
2015-04-23 12:59:20 -0430

    .

--- modulos/act_operador.php
+++ modulos/act_operador.php
@@ -4,6 +4,10 @@
 		$id_operador = $_GET['id_operador'];
 		$operador = $consultasbd->select($tabla='tbl_operador',$campos='*',$where=' WHERE id_operador=\''.$id_operador.'\'');
 		$operador = $consultasbd->fetch_array($operador);
+
+		// consulta privilegios
+		$sql_priv = $consultasbd->select($table='tbl_privilegios',$campos='*',$where='WHERE id_privilegio=\''.$operador['id_privilegio'].'\'');
+		$res_priv = $consultasbd->fetch_array($sql_priv);
 	}
 ?>
 <div class="col-sm-9 col-md-10">
@@ -16,32 +20,40 @@
     		<div class="row">
     			<div class="col-lg-3">
     				<label for="txt_cedula">Cedula&nbsp;&nbsp;<i class="glyphicon glyphicon-credit-card"></i></label>
-	    			<input type="text" name="txt_cedula" id="txt_cedula" class="form-control" title="Cedula" />	
+	    			<input type="text" name="txt_cedula" id="txt_cedula" class="form-control" title="Cedula" value="<?php echo $operador['cedula']; ?>" />	
     			</div>
     		</div>
     		<br/>
 	    	<div class="row">
 	    		<div class="col-lg-6">
 	    			<label for="txt_nombre">Nombre&nbsp;&nbsp;<i class="glyphicon glyphicon-user"></i></label>
-	    			<input type="text" name="txt_nombre" id="txt_nombre" class="form-control" title="Nombre" />
+	    			<input type="text" name="txt_nombre" id="txt_nombre" class="form-control" title="Nombre" value="<?php echo $operador['nombre']; ?>" />
 	    		</div>
 	    		<div class="col-lg-6">
 	    			<label for="txt_apellido">Apellido&nbsp;&nbsp;<i class="glyphicon glyphicon-user"></i></label>
-	    			<input type="text" name="txt_apellido" id="txt_apellido" class="form-control" title="Apellido" />
+	    			<input type="text" name="txt_apellido" id="txt_apellido" class="form-control" title="Apellido" value="<?php echo $operador['apellido']; ?>" />
 	    		</div>
 	    	</div>
 	    	<br/>
 	    	<div class="row">
 	    		<div class="col-lg-6">
 	    			<label for="txt_privilegio">Privilegio&nbsp;&nbsp;<i class="glyphicon glyphicon-star"></i></label>
-	    			<input type="text" name="txt_privilegio" id="txt_privilegio" class="form-control" title="Privilegio" />
+	    			<input type="text" name="txt_privilegio" id="txt_privilegio" class="form-control" title="Privilegio" value="<?php echo $res_priv['privilegio']; ?>" />
 	    		</div>
 	    		<div class="col-lg-6">
-	    			<label for="txt_password">Contrase&ntilde;a&nbsp;&nbsp;<i class="glyphicon glyphicon-lock"></i></label>
+	    			<label for="txt_password">Contrase&ntilde;a&nbsp;&nbsp;<i class="glyphicon glyphicon-lock"></i>&nbsp;&nbsp;<i class="text-info pull-right"><small>Omitir&nbsp;si&nbsp;no&nbsp;desea&nbsp;actualizar</small>&nbsp;<i class="glyphicon glyphicon-info-sign"></i></i></label>
 	    			<input type="text" name="txt_password" id="txt_password" class="form-control" title="Clave de Acceso" />
 	    		</div>
 	    	</div>
 	    	<br/>
+	    	<div class="row">
+	    		<div class="col-lg-12 text-right">
+	    			<div class="pull-right">
+	    				<button type="button" class="btn btn-muted">Cancelar</button>
+	    				<button type="submit" class="btn btn-danger">Actualizar</button>
+	    			</div>
+	    		</div>
+	    	</div>
     	</form>
     </div>
     <div class="panel-footer text-center">
